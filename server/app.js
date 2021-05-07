533f006e38524156501ae0d9f25bdf6cae2d5e81
Sam <samuelfmatias@ua.pt>
2021-05-07 17:18:21 +0100

    Conexão com bd em localhost

--- server/app.js
+++ server/app.js
@@ -1,3 +1,29 @@
-import express from "express";
+const express = require('express');
 
 const app = express();
+
+
+const mysql = require('mysql2')
+
+const db = mysql.createConnection({
+    user: 'root',
+    host: 'localhost',
+    password: '',
+    database: 'mydb'
+})
+
+app.get('/', (req, res) => {
+    res.send('We are on home')
+})
+app.get('/insert', (req, res) => {
+    db.query('INSERT INTO user (email, password, name, school, role_idrole) VALUES ("teste123", "123", "sam", "abel botelho", 0)', (err, result) => {
+        if (err) {
+            console.log(err);
+        }
+        res.send(result)
+    })
+})
+
+app.listen(3001, () => {
+    console.log('Server is running on port 3001');
+});
