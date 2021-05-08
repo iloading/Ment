5308255c6a3f140020adeb2fd8714bea7b5350e1
Sam <samuelfmatias@ua.pt>
2021-05-08 22:49:36 +0100

    Progress: link BE to FE
    Incompleto - falta completar o forms e deteção de erros no preenchimento. Sistema de JWT e autenticação

--- client/src/pages/login.js
+++ client/src/pages/login.js
@@ -1,7 +1,7 @@
 import loginImg from "../img/loginImg.png";
 import icon_mail from "../img/icon_mail.png";
 import icon_cadiado from "../img/icon_cadiado.png";
-
+import { Formik, Form, Field, ErrorMessage } from 'formik';
 
 function Login() {
     return (
@@ -10,38 +10,40 @@ function Login() {
                 <img src={loginImg} alt="" />
             </div>
             <div className="formatoForm">
-                <form>
-                    <section>
-                        <span className="formatoTitulo">Iniciar sessão</span>
-                    </section>
-                    <section>
-                        <span className="formatoLables">e-mail</span>
-                        <div className="formatoInput"> 
-                            <img src={icon_mail} alt="" />
-                            <input required type="email" placeholder="ex: joana.silva12@gmail.com"/>
-                        </div>
-                    </section>    
-                    <section>
-                        <span className="formatoLables">palavra-passe</span>
-                        <div className="formatoInput"> 
-                            <img src={icon_cadiado} alt="" />
-                            <input required type="email" placeholder="*********"/>
-                        </div>
-                    </section>
+                <Formik >
+                    <Form>
+                        <section>
+                            <span className="formatoTitulo">Iniciar sessão</span>
+                        </section>
+                        <section>
+                            <span className="formatoLables">e-mail</span>
+                            <div className="formatoInput">
+                                <img src={icon_mail} alt="" />
+                                <input required type="email" placeholder="ex: joana.silva12@gmail.com" />
+                            </div>
+                        </section>
+                        <section>
+                            <span className="formatoLables">palavra-passe</span>
+                            <div className="formatoInput">
+                                <img src={icon_cadiado} alt="" />
+                                <input required type="password" placeholder="*********" />
+                            </div>
+                        </section>
 
-                    <section className="botaoSubmit">
-                        <button type="submit">Login</button>
-                    </section>
+                        <section className="botaoSubmit">
+                            <button type="submit">Login</button>
+                        </section>
 
-                    <section className="textoFim">
-                        <span>Ainda não tem conta? Registe-se aqui</span>
-                    </section>
-                
-                </form>
+                        <section className="textoFim">
+                            <span>Ainda não tem conta? Registe-se aqui</span>
+                        </section>
+
+                    </Form>
+                </Formik>
             </div>
         </div>
-      );
-    
+    );
+
 }
 
 export default Login;
