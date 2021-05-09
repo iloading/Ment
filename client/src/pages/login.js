fe1521328d792c75da03be4e42f1d5052c72a59f
Mike <miguelvfilipe@ua.pt>
2021-05-09 17:03:16 +0100

    Css Grid Login Pag

--- client/src/pages/login.js
+++ client/src/pages/login.js
@@ -6,41 +6,38 @@ import icon_cadiado from "../img/icon_cadiado.png";
 function Login() {
     return (
         <article>
-            <div className="loginImg">
-                <img src={loginImg} alt="" />
-            </div>
-            <div className="formatoForm">
-                <form>
-                    <section>
-                        <span className="formatoTitulo">Iniciar sessão</span>
-                    </section>
-                    <section>
-                        <label className="formatoLables">e-mail</label>
-                        <div className="formatoInput">
-                            <img src={icon_mail} alt="" />
-                            <input required type="email" placeholder="ex: joana.silva12@gmail.com" />
-                        </div>
-                    </section>
-                    <section>
-                        <label className="formatoLables">palavra-passe</label>
-                        <div className="formatoInput">
-                            <img src={icon_cadiado} alt="" />
-                            <input required type="password" placeholder="*********" />
-                        </div>
-                    </section>
+           <div className="loginImg">
+               <img src={loginImg} alt="" />
+           </div>
+          
+           <form>
+               <section>
+                   <span>Iniciar sessão</span>
+               </section>
+               <section>
+                   <label>e-mail</label>
+                   <div>
+                       <img src={icon_mail} alt="" />
+                       <input required type="email" placeholder="ex: joana.silva12@gmail.com" />
+                   </div>
+               </section>
+               <section>
+                   <label>palavra-passe</label>
+                   <div>
+                       <img src={icon_cadiado} alt="" />
+                       <input required type="password" placeholder="*********" />
+                   </div>
+               </section>
+               <section className="botaoSubmit">
+                   <button type="submit">Login</button>
+               </section>
+               <section>
+                   <span>Ainda não tem conta? Registe-se aqui</span>
+               </section>
+           </form>
+       </article>
 
-                    <section className="botaoSubmit">
-                        <button type="submit">Login</button>
-                    </section>
-
-                    <section className="textoFim">
-                        <span>Ainda não tem conta? Registe-se aqui</span>
-                    </section>
-
-                </form>
-
-            </div>
-        </article>
+        
     );
 
 }
