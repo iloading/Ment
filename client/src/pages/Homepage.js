66d7c32c71c97b444d4bf551e17417576c0818f4
David Riboira <david.riboira@ua.pt>
2021-05-20 14:45:27 +0100

    Update Homepage

--- client/src/pages/Homepage.js
+++ client/src/pages/Homepage.js
@@ -1,7 +1,9 @@
 import React from 'react'
 import navLogo from "../img/logo.png";
 import home1 from "../img/home1.png";
-import { useHistory } from "react-router-dom"
+import imgBanco from "../img/imgBanco.png";
+import comofuncionaHome from "../img/comofuncionaHome.png";
+import { useHistory, Link } from "react-router-dom"
 
 function Homepage() {
     let history = useHistory();
@@ -30,6 +32,34 @@ function Homepage() {
                     <button type="button" className="botaoRegisto">Quero-me registar!</button>
 
                     <img src={home1} alt="" />
+
+                </section>
+                <section id="apresentacaoBanco">
+                    <h2>Um banco de narrativas na sua mão</h2>
+
+
+                    <img src={imgBanco} alt="" />
+
+                </section>
+                <section id="comoFunciona">
+                    <h2>Como funciona:</h2>
+
+
+                    <img src={comofuncionaHome} alt="" />
+
+                    <div id="texto2">
+                        <p>Na ment pode:</p>
+                        <ul>
+                            <li>Visitar vários projetos de mentoria entre pares realizados em todo o país, de vários disciplinas, anos de escolaridade e até matérias específicas;</li>
+                            <li>Agilizar processo de criação, com assistência em todas as etapas;</li>
+                            <li>Gerir membros, através da organização por equipas;</li>
+                            <li>Visitar outros projetos já realizados e encontrar a inspiração que tanto precisa!</li>
+                        </ul>
+
+                    </div>
+
+
+                    <Link to="/123">Parece complicado? Veja como funciona passo-a-passo clicando aqui.</Link>
 
                 </section>
             </article>
