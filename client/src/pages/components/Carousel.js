1ad5a9ca2730ba75c12b4d3ad34810907f3b7e72
Sam <samuelfmatias@ua.pt>
2021-05-29 18:47:13 +0100

    Carrossel Homepage fixed

--- client/src/pages/components/Carousel.js
+++ client/src/pages/components/Carousel.js
@@ -8,36 +8,38 @@ import carouselCard1 from "../../img/carouselCards1.png";
 
 
 
-
+/* VER DOCUMENTAÇÃO PARA ALTERAR COMPORTAMENTO DO CARROSSEL https://react-slick.neostack.com/docs/example/simple-slider */
 function Carousel() {
     var settings = {
         dots: true,
+        fade: false,
         infinite: true,
         speed: 500,
-        slidesToShow: 4,
-        slidesToScroll: 4,
+        slidesToShow: 3,
+        slidesToScroll: 1,
         initialSlide: 0,
         arrows: false,
+        autoplay: true,
+        pauseOnHover: true,
         responsive: [
             {
                 breakpoint: 1024,
                 settings: {
-                    slidesToShow: 3,
-                    slidesToScroll: 3,
-                    infinite: true,
-                    dots: true
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    fade: true,
+
                 }
             },
             {
                 breakpoint: 600,
-                settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 2,
-                    initialSlide: 2
-                }
-            },
-            {
-                breakpoint: 480,
                 settings: {
                     slidesToShow: 1,
                     slidesToScroll: 1
@@ -87,7 +89,7 @@ function Carousel() {
             </Slider>
         </div >
 
-       
+
 
 
 
