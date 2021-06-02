a70800014cd37b1fe1c09b22c7ec10824899b7d3
Sam <samuelfmatias@ua.pt>
2021-06-02 11:17:47 +0100

    Update - Etapa 4 Formatada

--- client/src/pages/Registo/RegistoEtapa4.js
+++ client/src/pages/Registo/RegistoEtapa4.js
@@ -1,5 +1,4 @@
 import registo5Img from "../../img/registo/registo5Img.png";
-import icon_nome from "../../img/icons/icon_nome.png";
 import icon_dropdown from "../../img/icons/icon_dropdown.png";
 import setaAtras from "../../img/setaAtras.png";
 import { Field, ErrorMessage } from 'formik';
@@ -36,12 +35,12 @@ function RegistoEtapa4({ setEtapa, dados, setDados }) {
                     </div>
                     <img src={registo5Img} alt="registo quinta imagem" />
                 </header>
-                <div className="formulario">
+                <div className="formulario" id={"etapa4"}>
                     <section className="tituloPrincipal">
                         <label>Informações</label>
                     </section>
                     <section className="paragrafo">
-                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ab quos at nostrum nemo earum obcaecati voluptas consectetur, qui recusandae ut delectus harum nobis aliquid, odit ex deserunt laudantium, assumenda voluptatibus?</p>
+                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ab quos at nostrum nemo earum obcaecati voluptas consectetur.</p>
                     </section>
                     <section className="inputFormulario">
 
@@ -49,19 +48,25 @@ function RegistoEtapa4({ setEtapa, dados, setDados }) {
                         {/* Falta meter estilos nestes erros */}
 
                         <div>
-                            <img src={icon_nome} alt="icone nome" />
+
                             <Field placeholder="ex: Escola Random Random" name="escola" id="inputEscola" type="text" />
                         </div>
-                        <ErrorMessage name="email" component="p" />
+                        <div className="error">
+                            <ErrorMessage name="school" component="p" />
+                        </div>
+
                     </section>
                     <section className="selectFormulario">
-                        <label>Disciplina --- temos que ver isto pq os alunos ao fazer o registo não vão ter de selecionar disciplina</label>
+                        <label>Disciplina</label>
                         <div>
                             <img src={icon_dropdown} alt="icone dropdown" />
                             <Field as="select" name="disciplina">
                                 <option value="mat">Mat</option>
                                 <option value="ing">ING</option>
                             </Field>
+                        </div>
+                        <div className="error">
+                            <ErrorMessage name="school" component="p" />
                         </div>
                     </section>
 
