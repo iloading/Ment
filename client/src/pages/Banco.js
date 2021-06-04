59e88d24389803915fa2e300c89a9b90a0d43059
Miguel <miguelvfilipe@ua.pt>
2021-06-04 20:07:40 +0100

    update - banco praticamente feito

--- client/src/pages/Banco.js
+++ client/src/pages/Banco.js
@@ -3,7 +3,7 @@ import Navbar from "./components/Navbar"
 
 import iconBanco from "../img/icons/icon_banco.png";
 
-import setaDireita from "../img/setaDireita.png";
+import setaDireita from "../img/setaDireita_cor.png";
 import iconSessoes from "../img/icons/icon_sessoes.png";
 import iconOrdenar from "../img/icons/icon_ordenar.png";
 import iconFiltro from "../img/icons/icon_filtro.png";
@@ -39,6 +39,48 @@ function Banco() {
 
                 </div>
 
+                <div className="cardSessoes">
+
+                    <div>
+                        <img src={iconSessoes} alt="" />
+                        <div className="textoCards">
+                            <label>Os 3 porquinhos </label>
+                            <p>Equipa Avila Crew</p>
+                            <span>8º ano | Past Tense</span>
+                        </div>
+
+                        <img src={setaDireita} alt="" />
+                    </div>
+
+                </div>
+                <div className="cardSessoes">
+
+                    <div>
+                        <img src={iconSessoes} alt="" />
+                        <div className="textoCards">
+                            <label>Os 3 porquinhos </label>
+                            <p>Equipa Avila Crew</p>
+                            <span>8º ano | Past Tense</span>
+                        </div>
+
+                        <img src={setaDireita} alt="" />
+                    </div>
+
+                </div>
+                <div className="cardSessoes">
+
+                    <div>
+                        <img src={iconSessoes} alt="" />
+                        <div className="textoCards">
+                            <label>Os 3 porquinhos </label>
+                            <p>Equipa Avila Crew</p>
+                            <span>8º ano | Past Tense</span>
+                        </div>
+
+                        <img src={setaDireita} alt="" />
+                    </div>
+
+                </div>
                 <div className="cardSessoes">
 
                     <div>
