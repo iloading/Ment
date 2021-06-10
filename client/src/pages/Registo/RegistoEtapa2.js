4db0b63c16c61b1de559f2c9497d342026482851
Sam <samuelfmatias@ua.pt>
2021-06-10 18:00:25 +0100

    Update - React-select Implementado

--- client/src/pages/Registo/RegistoEtapa2.js
+++ client/src/pages/Registo/RegistoEtapa2.js
@@ -1,14 +1,15 @@
 import registoImg from "../../img/registo/registoImg.png";
 import setaAtras from "../../img/setaAtras.png";
 /* import icon_nome from "../../img/icon_nome.png"; */
-import icon_dropdown from "../../img/icons/icon_dropdown.png";
+/* import icon_dropdown from "../../img/icons/icon_dropdown.png"; */
 import { Field, ErrorMessage } from 'formik';
-
-
+import { colourStyles } from './selectStyle';
+//Fromulario
+import Select from 'react-select'
 import { Formik, Form } from "formik";
 import * as Yup from 'yup';
 
-import { useEffect } from "react"
+import { useEffect, useCallback } from "react"
 
 
 
@@ -18,55 +19,70 @@ function RegistoEtapa2({ setEtapa, dados, setDados, validadeFormulario2, setvali
         window.scrollTo(0, 0);
     }, [])
 
+
+
+    const options = [
+
+        { value: 0, label: 'Aluno Mentor' },
+        { value: 1, label: 'Professor' }
+    ]
+
+
     let initialValues
 
     /* Desta forma se o utilizador voltar a tras durante o registo, os dados ficam guardados nos campos corretos */
 
-    (dados.name || dados.role) ?
+    (dados.name) ?
         initialValues = {
             name: dados.name,
-            role: dados.role,
-
         } : initialValues = {
             name: "",
-            role: "",
         }
 
 
     const camposValidador = Yup.object().shape({
-        name: Yup.string().required("Este campo é obrigatório").min(3, 'Por favor insira o seu primeiro e último nomes, o número mínimo de carateres é 3').max(250, 'O número máximo de carateres é 250'),
-        role: Yup.number().required("Este campo é obrigatório").positive('Selecione um cargo').integer('Selecione um cargo').min(0, 'Selecione um cargo').max(1, 'Selecione um cargo'),
+        name: Yup.string().required("Este campo é obrigatório").min(4, 'Por favor insira o seu primeiro e último nomes, o número mínimo de carateres é 4').max(250, 'O número máximo de carateres é 250'),
+        /*  role: Yup.number().required("Este campo é obrigatório").positive('Selecione um cargo').integer('Selecione um cargo').min(0, 'Selecione um cargo').max(1, 'Selecione um cargo'), */
 
     })
 
 
-    const validar = () => {
+    const validar = useCallback(
+        () => {
+            camposValidador.validate({
+                name: document.getElementById('inputName').value,
+                /* role: document.getElementById('inputRole').value, */
 
-        camposValidador.validate({
-            name: document.getElementById('inputName').value,
-            role: document.getElementById('inputRole').value,
+            }).then(function () {
+                if (dados.role === 1 || dados.role === 0) {
+                    setvalidadeFormulario2(true)
+                }
 
-        }).then(function () {
 
-            setvalidadeFormulario2(true)
+            }).catch((e) => {
+                setvalidadeFormulario2(false);
 
-        }).catch((e) => {
-            setvalidadeFormulario2(false);
-
-        });
-    }
-
+            });
+        },
+        [setvalidadeFormulario2, camposValidador, dados.role],
+    )
     //Quando todos os campos são devidamente preenchidos e válidos, o botão de próxima etapa fica disponível
 
 
 
 
+    const handleChange = (e) => {
+        setDados({ ...dados, role: e.value })
 
+    }
 
+    useEffect(() => {
+        validar()
+
+    }, [dados.role, validar])
 
     const onSubmit = (data) => {
-
-        setDados({ ...dados, name: data.name, role: data.role })
+        setDados({ ...dados, name: data.name })
         setEtapa(3)
     }
 
@@ -105,16 +121,14 @@ function RegistoEtapa2({ setEtapa, dados, setDados, validadeFormulario2, setvali
                         </div>
 
                     </section>
-                    <section className="inputFormulario selectFormulario">
+                    <section className="">
                         <label>Cargo</label>
-                        <div>
-                            <img src={icon_dropdown} alt="icone dropdown" />
-                            <Field as="select" name="role" required id='inputRole' onInput={validar}>
-                                <option value="" disabled hidden selected>Selecionar Cargo</option>
-                                <option value={0}>Aluno Mentor</option>
-                                <option value={1}>Professor</option>
-                            </Field>
-                        </div>
+
+                        {/* <img src={icon_dropdown} alt="icone dropdown" /> */}
+
+                        <Select options={options} required id='inputRole' className='react-select-form' isSearchable={false} styles={colourStyles} placeholder={'Selecionar Cargo'} onChange={handleChange} value={options.find(obj => obj.value === dados.role)}> </Select>
+
+
                         <div className="error">
                             <ErrorMessage name="role" component="p" />
                         </div>
