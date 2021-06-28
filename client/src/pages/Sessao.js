864e7250fb67f193755fc357e0ce8b0e818b4334
Miguel <miguelvfilipe@ua.pt>
2021-06-28 11:02:56 +0100

    update - conteudos html - pag sessao

--- client/src/pages/Sessao.js
+++ client/src/pages/Sessao.js
@@ -21,71 +21,29 @@ function Sessao() {
 
 
             <section id="main" className="conteudoMain">
-
-                <h1>ola</h1>
-                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Possimus totam est facilis velit, recusandae quis inventore officiis quia exercitationem corrupti tempore nobis fuga eum laboriosam hic excepturi cumque? Molestias, commodi!</p>
-                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Possimus totam est facilis velit, recusandae quis inventore officiis quia exercitationem corrupti tempore nobis fuga eum laboriosam hic excepturi cumque? Molestias, commodi!</p>
-                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Possimus totam est facilis velit, recusandae quis inventore officiis quia exercitationem corrupti tempore nobis fuga eum laboriosam hic excepturi cumque? Molestias, commodi!</p>
-                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Possimus totam est facilis velit, recusandae quis inventore officiis quia exercitationem corrupti tempore nobis fuga eum laboriosam hic excepturi cumque? Molestias, commodi!</p>
-                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Possimus totam est facilis velit, recusandae quis inventore officiis quia exercitationem corrupti tempore nobis fuga eum laboriosam hic excepturi cumque? Molestias, commodi!</p>
-                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Possimus totam est facilis velit, recusandae quis inventore officiis quia exercitationem corrupti tempore nobis fuga eum laboriosam hic excepturi cumque? Molestias, commodi!</p>
-                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Possimus totam est facilis velit, recusandae quis inventore officiis quia exercitationem corrupti tempore nobis fuga eum laboriosam hic excepturi cumque? Molestias, commodi!</p>
-                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Possimus totam est facilis velit, recusandae quis inventore officiis quia exercitationem corrupti tempore nobis fuga eum laboriosam hic excepturi cumque? Molestias, commodi!</p>
-                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Possimus totam est facilis velit, recusandae quis inventore officiis quia exercitationem corrupti tempore nobis fuga eum laboriosam hic excepturi cumque? Molestias, commodi!</p>
-                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Possimus totam est facilis velit, recusandae quis inventore officiis quia exercitationem corrupti tempore nobis fuga eum laboriosam hic excepturi cumque? Molestias, commodi!</p>
-                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Possimus totam est facilis velit, recusandae quis inventore officiis quia exercitationem corrupti tempore nobis fuga eum laboriosam hic excepturi cumque? Molestias, commodi!</p>
-                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Possimus totam est facilis velit, recusandae quis inventore officiis quia exercitationem corrupti tempore nobis fuga eum laboriosam hic excepturi cumque? Molestias, commodi!</p>
-                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Possimus totam est facilis velit, recusandae quis inventore officiis quia exercitationem corrupti tempore nobis fuga eum laboriosam hic excepturi cumque? Molestias, commodi!</p>
-                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Possimus totam est facilis velit, recusandae quis inventore officiis quia exercitationem corrupti tempore nobis fuga eum laboriosam hic excepturi cumque? Molestias, commodi!</p>
-                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Possimus totam est facilis velit, recusandae quis inventore officiis quia exercitationem corrupti tempore nobis fuga eum laboriosam hic excepturi cumque? Molestias, commodi!</p>
-                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Possimus totam est facilis velit, recusandae quis inventore officiis quia exercitationem corrupti tempore nobis fuga eum laboriosam hic excepturi cumque? Molestias, commodi!</p>
-                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Possimus totam est facilis velit, recusandae quis inventore officiis quia exercitationem corrupti tempore nobis fuga eum laboriosam hic excepturi cumque? Molestias, commodi!</p>
-
-                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Possimus totam est facilis velit, recusandae quis inventore officiis quia exercitationem corrupti tempore nobis fuga eum laboriosam hic excepturi cumque? Molestias, commodi!</p>
-                {/*
-
-
-
                 <div className="conteudoMid">
-
-
+                    <label>Descrição</label>
                     <div className="cardDescricao">
-
-                        <label>Descrição</label>
-
                         <div className="topInfo">
-
                             <div className="titulos">
                                 <label>Os 3 porquinhos </label>
                                 <p>Past Tense</p>
                                 <span>8º ano</span>
                             </div>
                             <img className="imgSessao" src={iconSessoes} alt="" />
-
                         </div>
-
                         <div className="bottomInfo">
                             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus temporibus consequuntur omnis. Veniam provident nam incidunt delectus dicta labore?</p>
-
-
-
                         </div>
-
-
                     </div>
-
-                    <div className="seccaoComents">
-
-                        <label>Descrição</label>
-
-
-
-                    </div>
-
 
                 </div>
-                */}
-
+                <div className="conteudoComents">
+                    <div className="seccaoComents">
+                        <label>Descrição</label>
+                    </div>
+                    <h1>ola</h1>
+                </div>
             </section>
 
 
