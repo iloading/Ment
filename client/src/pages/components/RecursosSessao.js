a20072aeca99630f1efecc1823c405de399ba57d
David Riboira <david.riboira@ua.pt>
2021-07-04 18:07:25 +0100

    update - pagina de sessao (Recursos e Detalhes) feito

--- client/src/pages/components/RecursosSessao.js
+++ client/src/pages/components/RecursosSessao.js
@@ -1,10 +1,41 @@
 import React from 'react'
+import iconDownload from "../../img/icons/icon_download.svg";
+
 
 function RecursosSessao() {
     return (
-        <div>
-            <h1>pisssa</h1>
-        </div>
+        <>
+            <div className="conteudoPlano">
+                <div className="ficheirosPrincipais">
+                    <label className="titulo1">Ficheiros principais</label>
+                    <div className="ficheiros">
+                        <div className="factos">
+                            <label className="factos_nome">Narrativa - "London Culture"</label>
+                            <img src={iconDownload} alt="" className="factos_icon" />
+                        </div>
+                        <div className="factos">
+                            <label className="factos_nome">Guião dos Mentores</label>
+                            <img src={iconDownload} alt="" className="factos_icon" />
+                        </div>
+                        <div className="factos">
+                            <label className="factos_nome">Material Multimédia</label>
+                            <img src={iconDownload} alt="" className="factos_icon" />
+                        </div>
+                    </div>
+                </div>
+                <div className="conteudosAdicionais">
+
+                    <label className="titulo2">Conteúdo adicional</label>
+                    <div className="ficheiros">
+                        <div className="factos">
+                            <label className="factos_nome">Exercícios Icebreakers</label>
+                            <img src={iconDownload} alt="" className="factos_icon" />
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </>
     )
 }
 
