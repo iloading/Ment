5bb170b42432e98cf886011d8a3df097805c86a7
Miguel <miguelvfilipe@ua.pt>
2021-07-08 16:17:43 +0100

    update - fix minhas equipas dashboard

--- client/src/pages/Perfil.js
+++ client/src/pages/Perfil.js
@@ -6,6 +6,7 @@ import iconTerminar from "../img/icons/icon_terminarSessao.svg";
 
 import { useContext } from 'react'
 import { useHistory } from 'react-router-dom'
+import { useSelector } from 'react-redux'
 import AuthContext from "../context/AuthContext";
 
 import { logout } from '../API'
@@ -22,70 +23,73 @@ function Perfil() {
         await getLoggedIn();
         history.push('/login')
     }
+    const { user, status } = useSelector(state => state.user)
 
     return (
         <article className="perfil">
 
+            {status === 'completed' &&
 
-
-            <section id="main" className="conteudoMain">
-
-                <div className="titulo">
-                    <img src={iconPerfil} alt="" className="iconTitulo" />
-                    <label className="tituloMain">O meu perfil</label>
-                </div>
-
-                <div className="infoPerfil">
-                    <img src={avatar} alt="" />
-                    <label>Joana Silva</label>
-                    <span>joanasilva.12@gmail.com</span>
-                </div>
-
-                <div className="conteudoBotoes">
+                <section id="main" className="conteudoMain">
 
                     <div className="titulo">
-                        <label>Conteúdo</label>
+                        <img src={iconPerfil} alt="" className="iconTitulo" />
+                        <label className="tituloMain">O meu perfil</label>
                     </div>
 
-
-                    <div className="botao">
-                        <div id="img_esquerda">
-                            <img src={favorito} alt="" />
-                        </div>
-                        <div id="texto_Direita">
-                            <label>Os meus favoritos</label>
-                        </div>
+                    <div className="infoPerfil">
+                        <img src={require(`../img/avatar/${user.url}`).default} alt="" />
+                        <label>{user.name}</label>
+                        <span>{user.email}</span>
                     </div>
 
+                    <div className="conteudoBotoes">
 
-                    <div className="titulo">
-                        <label>Gestão</label>
+                        <div className="titulo">
+                            <label>Conteúdo</label>
+                        </div>
+
+
+                        <div className="botao">
+                            <div id="img_esquerda">
+                                <img src={favorito} alt="" />
+                            </div>
+                            <div id="texto_Direita">
+                                <label>Os meus favoritos</label>
+                            </div>
+                        </div>
+
+
+                        <div className="titulo">
+                            <label>Gestão</label>
+                        </div>
+
+                        <div className="botao">
+                            <div id="img_esquerda">
+                                <img src={iconSettings} alt="" />
+                            </div>
+                            <div id="texto_Direita">
+                                <label>Definições</label>
+                            </div>
+                        </div>
+
+
+
+                        <div className="botao" onClick={logOutButton}>
+                            <div id="img_esquerda">
+                                <img src={iconTerminar} alt="" />
+                            </div>
+                            <div id="texto_Direita">
+                                <label>Terminar sessão</label>
+                            </div>
+                        </div>
+
+
                     </div>
 
-                    <div className="botao">
-                        <div id="img_esquerda">
-                            <img src={iconSettings} alt="" />
-                        </div>
-                        <div id="texto_Direita">
-                            <label>Definições</label>
-                        </div>
-                    </div>
+                </section>
 
-
-
-                    <div className="botao" onClick={logOutButton}>
-                        <div id="img_esquerda">
-                            <img src={iconTerminar} alt="" />
-                        </div>
-                        <div id="texto_Direita">
-                            <label>Terminar sessão</label>
-                        </div>
-                    </div>
-
-
-                </div>
-
-            </section>
+            }
 
 
 
