efcc9178295ddc11745c46b7279d6a14021bc9c7
Sam <samuelfmatias@ua.pt>
2021-07-08 16:46:28 +0100

    log

--- client/src/pages/components/MinhaEquipa.js
+++ client/src/pages/components/MinhaEquipa.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 
 function MinhaEquipa({ alias, name, schoolName, numero, equipa }) {
 
-    console.log(equipa);
+
     return (
         <>
             {equipa &&
