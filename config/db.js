e309ac149fcaad08e1b9bc3507de9c4582634443
Sam <samuelfmatias@ua.pt>
2021-07-09 18:59:10 +0100

    1

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 const mysql = require('mysql');
 var connection = mysql.createPool({
-    connectionLimit: 2,
+    connectionLimit: 10,
     user: 'bb634438aacb2d',
     host: 'eu-cdbr-west-01.cleardb.com',
     password: '1d38da54',
@@ -9,13 +9,13 @@ var connection = mysql.createPool({
 
 /* mysql://bb634438aacb2d:1d38da54@eu-cdbr-west-01.cleardb.com/heroku_acd41d977512a36?reconnect=true */
 
-connection.connect(function (err, suc) {
+/* connection.connect(function (err, suc) {
     if (err) {
         console.log('Error connecting to Database'.red);
     } else {
         console.log('Connected to Database'.green);
     }
-});
+}); */
 
 /* const { Prohairesis } = require('prohairesis')
 const env = require('../env');
