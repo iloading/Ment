c8d87e5ec2cdbdeeec153ff128038cb429886377
Sam <samuelfmatias@ua.pt>
2021-07-09 13:59:39 +0100

    FIXED CORS?

--- app.js
+++ app.js
@@ -8,14 +8,14 @@ const app = express();
 app.use(express.json());
 
 app.use(cors({
-    origin: true,
+    origin: ['https://ment-9ad64.web.app', '*'],
     credentials: true,
 }));
 // Add headers
 app.use(function (req, res, next) {
 
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'https://ment-9ad64.web.app');
+    res.setHeader('Access-Control-Allow-Origin', '*');
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
