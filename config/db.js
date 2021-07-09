514463bee78f2b39190f8c7c0ba6b8eaa06b63bb
Sam <samuelfmatias@ua.pt>
2021-07-09 12:14:43 +0100

    connection to db

--- config/db.js
+++ config/db.js
@@ -1,9 +1,9 @@
 const mysql = require('mysql');
 var connection = mysql.createConnection({
-    user: 'root',
-    host: 'localhost',
-    password: '',
-    database: 'mydb'
+    user: 'bc6050a16ba73c',
+    host: 'us-cdbr-east-04.cleardb.com',
+    password: 'd16badb2',
+    database: 'heroku_8b30da5ebf6c579'
 });
 
 connection.connect(function (err) {
