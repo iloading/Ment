ba72d06b7e5f1cb244d979f3ff785d7172008fd9
Sam <samuelfmatias@ua.pt>
2021-07-09 13:03:36 +0100

    no fix yet

--- app.js
+++ app.js
@@ -10,6 +10,25 @@ app.use(cors({
     origin: true,
     credentials: true,
 }));
+// Add headers
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'https://ment-9ad64.web.app/');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
 app.use(cookieParser());
 
 
