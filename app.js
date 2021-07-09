74ae762241fafc05bc6b8d43573959bcefe8f138
Sam <samuelfmatias@ua.pt>
2021-07-09 11:59:47 +0100

    cors ?

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors({
-    origin: ['http://localhost:3000'],
+    origin: ['https://ment-ua.herokuapp.com/'],
     credentials: true,
 }));
 app.use(cookieParser());
