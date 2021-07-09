8c339284f814633958f3c74429650a94344f81b1
Sam <samuelfmatias@ua.pt>
2021-07-09 14:03:53 +0100

    c

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use(cors({
 app.use(function (req, res, next) {
 
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'https://ment-9ad64.web.app');
+    res.setHeader('Access-Control-Allow-Origin', 'https://ment-9ad64.web.app/');
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
