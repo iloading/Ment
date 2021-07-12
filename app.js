60f38616331eac8dff695d381e01e4be9a34acd9
Sam <samuelfmatias@ua.pt>
2021-07-12 17:07:11 +0100

    cors

--- app.js
+++ app.js
@@ -6,10 +6,10 @@ const db = require('./config/db');
 const app = express();
 
 app.use(express.json());
-app.use(cors({
+/* app.use(cors({
     origin: ['http://localhost:3000'],
     credentials: true,
-}));
+})); */
 app.use(cookieParser());
 
 
