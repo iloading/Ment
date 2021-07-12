77315ea4609d3589c05ee94dfa16bff5887a3c76
Sam <samuelfmatias@ua.pt>
2021-07-12 17:52:17 +0100

    db change

--- config/db.js
+++ config/db.js
@@ -1,13 +1,14 @@
 const mysql = require('mysql');
-var connection = mysql.createConnection({
+var connection = mysql.createPool({
+    connectionLimit: 10,
     user: 'bc8172db4b5fd4',
     host: 'eu-cdbr-west-01.cleardb.com',
     password: 'a7c2feb2',
     database: 'heroku_866dbd40868d539'
 });
 
-connection.connect(function (err) {
+/* connection.connect(function (err) {
     if (err) throw err;
 });
-
+ */
 module.exports = connection;
